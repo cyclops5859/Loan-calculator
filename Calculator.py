86bd2ee61893b8be1e4087a5f76c9665417b23f8
Gen-X-Cyclops <67413980+cyclops5859@users.noreply.github.com>
2022-01-29 03:56:51 -0800

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,44 @@
+import time
+
+monthlyPayment = 0
+loan = 0
+interestRate = 0
+numberOfPayments = 0
+loanDurationInYears = 0
+
+print ("Welcome to Cyclops bank!")
+print ("")
+time.sleep(1)
+print(r"""
+    ----------------------------------------------------------------
+     _______           _______  _        _______  _______  _______   
+    (  ____ \|\     /|(  ____ \( \      (  ___  )(  ____ )(  ____ \  
+    | (    \/( \   / )| (    \/| (      | (   ) || (    )|| (    \/  
+    | |       \ (_) / | |      | |      | |   | || (____)|| (_____   
+    | |        \   /  | |      | |      | |   | ||  _____)(_____  )  
+    | |         ) (   | |      | |      | |   | || (            ) |  
+    | (____/\   | |   | (____/\| (____/\| (___) || )      /\____) |  
+    (_______/   \_/   (_______/(_______/(_______)|/       \_______)
+
+    ----------------------------------------------------------------
+                                                                """)
+time.sleep(1)
+
+print ("Let's calculate Your loan....")
+print ("")
+time.sleep(1)
+
+strLoan = input("Enter your loan amount ($): ")
+strInterestRate = input("What's the interest rate on the loan amount (%):  ")
+strLoanDurationInYears = input("Estimted time for payback of loan (Years): " )
+
+loanDurationInYears = float(strLoanDurationInYears)
+loan = float(strLoan)
+interestRate = float(strInterestRate)
+
+numberOfPayments = loanDurationInYears*12
+
+monthlyPayment = loan * interestRate * (1+ interestRate) * numberOfPayments \
+    / ((1 + interestRate) * numberOfPayments -1)
+
+print("Your monthly payment will be ($/Year) : ", round(monthlyPayment))
